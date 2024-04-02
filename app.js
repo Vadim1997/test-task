e3e59eaaaae272bd8fd965ff030756ac6c469e2b
Vadim Fedonyuk <mr.fedonyuk777@gmail.com>
2024-04-02 13:49:20 +0300

    add read button

--- app.js
+++ app.js
@@ -8,11 +8,13 @@ function createProduct(productsArray) {
         const productWrapper = document.createElement('div');
         const title = document.createElement('h2');
         const descriptionProduct = document.createElement('p');
+        const btnReadMore = document.createElement('button');
         const price = document.createElement('p');
+        btnReadMore.textContent  = "Read More";
         title.textContent = product.title;
         title.className = 'products-container__product__title';
         descriptionProduct.className = 'products-container__product__description';
-        descriptionProduct.textContent = product.description;
+        descriptionProduct.textContent = product.description.substring(0, 100) + "...";
         price.textContent = product.price + " $";
         productWrapper.className = 'products-container__product';
         const imgProduct = document.createElement('img');
@@ -22,10 +24,21 @@ function createProduct(productsArray) {
            imgProduct.onerror = function() {
                 imgProduct.src = 'https://placehold.co/600x400';
             }
+            btnReadMore.addEventListener('click', () => {
+                if(descriptionProduct.classList.contains('expanded')){
+                    descriptionProduct.textContent = product.description.substring(0, 100)+ "...";
+                    btnReadMore.textContent = 'Read more';
+                } else {
+                    descriptionProduct.textContent = product.description;
+                    btnReadMore.textContent = 'Read less';
+                }
+                descriptionProduct.classList.toggle('expanded');
+            });
         productsContainerEL.append(productWrapper);
         productWrapper.append(imgProduct);
         productWrapper.append(title);
         productWrapper.append(descriptionProduct);
+        productWrapper.append(btnReadMore);
         productWrapper.append(price);        
     });
 } 
